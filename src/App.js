57f53e37bc1787079d86ddfc96a45089a95374ea
ramasapto <ramasapto3@gmail.com>
2021-01-17 17:22:23 +0700

    update structur folder && assets

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
+
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import Dashboard from './views/dashboard';
+import { ThemeProvider } from '@material-ui/core';
+import theme from 'src/theme';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
-
-const Index = props => {
-const classes = useStyles();
-
+const App = props => {
   return(
-    <>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-      </Grid>
-    </>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route path="/" name="Home" component={Dashboard}/>
+        </Switch>
+      </ThemeProvider>
+    </Router>
   )
 }
 
-export default Index;
+export default App;

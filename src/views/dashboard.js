f2c222f875d8ac861c34ccb09e804871471b2740
ramasapto <ramasapto3@gmail.com>
2021-01-17 21:54:41 +0700

    update grid

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -1,10 +1,27 @@
 import './style/dashboard.css';
 import Nav from './nav';
+import {
+  Container,
+  makeStyles,
+} 
+from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+     root: {
+       minHeight: '100%',
+     }
+}));
+
 const Dashboard = () =>{
+  const styles = useStyles()
     return (
-        <div className="section1 bg-grey">
-            <Nav/>
-        </div>
+          <div className="section1 bg-grey">
+            <Container
+              maxWidth="md"
+            >
+              <Nav/>
+            </Container>
+          </div>
     )
 }
 
--- src/views/nav.js
+++ src/views/nav.js
@@ -1,5 +1,9 @@
 import Logo from '../assets/logo.svg';
 import './style/nav.css';
+import {
+  makeStyles,
+  Grid
+} from '@material-ui/core';
 
 const nav = [
   {
@@ -16,23 +20,37 @@ const nav = [
   }
 ]
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow:1,
+  }
+}));
+
+
 const Nav = () =>{
+  const classes = useStyles();
     return (
-      <nav className="nav">
-        <img
-        src={Logo}
-        className="nav-logo"
-        />
-        <ul>
-        { nav.map((content, index) => 
-          <li key={index}>
-            <a>
-              {content.title}
-            </a>
-          </li>
-          )
-        }
-        </ul>
+      <nav className={`nav ${classes.root}`}>
+        <Grid containter >
+          <Grid item xs={2}>
+            <img
+            src={Logo}
+            className="nav-logo"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <ul>
+            { nav.map((content, index) => 
+              <li key={index} >
+                <a>
+                  {content.title}
+                </a>
+              </li>
+              )
+            }
+            </ul>
+          </Grid>
+        </Grid>
       </nav>
     )
 }

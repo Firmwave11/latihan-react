67630b0a10c64d465fd3ac8f0ff50760301c98d8
ramasapto <ramasapto3@gmail.com>
2021-02-06 16:18:16 +0700

    update package

--- src/views/contentInfo.js
+++ src/views/contentInfo.js
@@ -6,23 +6,35 @@ import {
   Grid
 }
   from '@material-ui/core';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
+  titleTypo:{
+    fontWeight:700
+  },
+  subtitle:{
+    fontWeight:800,
+    fontSize:25,
+    color:"#909090",
+  },
   titleHi: {
     marginBottom: 54
   }
+
 }));
 const ContentInfo = () => {
   const classes = useStyles();
   return (
     <Grid container alignItems="center">
       <Grid item lg={6}>
-        <Typography variant="h5" className={classes.titleHi}>
+        <Typography variant="h5" className={clsx(classes.titleHi,classes.titleTypo)}>
           Hi, I am
           </Typography>
-        <Typography variant="h3" className={classes.titleHi}>
-          Rama Sapto <br />
-            Front-end Developer / UI Designer
+        <Typography variant="h3" className={classes.titleTypo}>
+          Donni Septian Silalahi 
+          </Typography>
+          <Typography variant="h4" className={classes.subtitle}>
+            CEO / Matahari
           </Typography>
       </Grid>
     </Grid>

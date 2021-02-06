0a4e3f128479371addaf1b607c22d5811c562250
ramasapto <ramasapto3@gmail.com>
2021-02-06 17:33:57 +0700

    update content

--- src/views/contentInfo.js
+++ src/views/contentInfo.js
@@ -10,6 +10,7 @@ import {
 import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import ProfileImg from '../assets/image 17.svg'
 import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
@@ -24,9 +25,19 @@ const useStyles = makeStyles((theme) => ({
   titleHi: {
     marginBottom: 54
   },
-  socialMedia: {
-    display:'flex',
-
+  socialMediaBtn: {
+    display:'inline-flex',
+    width:61,
+    height:56,
+    backgroundColor:"#C4C4C4",
+    marginRight:31,
+    boxShadow:"0px 4px 4px rgba(0, 0, 0, 0.25)",
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:126,
+    '& svg':{
+      fontSize:35
+    }
   }
 }));
 const ContentInfo = () => {
@@ -48,22 +59,35 @@ const ContentInfo = () => {
   return (
     <Grid container alignItems="center">
       <Grid item lg={6}>
-        <Typography variant="h5" className={clsx(classes.titleHi,classes.titleTypo)}>
-          Hi, I am
-          </Typography>
-        <Typography variant="h3" className={classes.titleTypo}>
-          Donni Septian Silalahi 
-        </Typography>
-        <Typography variant="h4" className={classes.subtitle}>
-          CEO / Matahari
-        </Typography>
-        <div className={classes.socialMedia}>
-          {socialMedias.map((sm)=>
-          <a key={sm.link} href={sm.link}>
-            {sm.icons}
-          </a>
-          )}
-        </div>
+        <Grid container>
+          <Grid item lg={12}>
+            <Typography variant="h5" className={clsx(classes.titleHi,classes.titleTypo)}>
+              Hi, I am
+            </Typography>
+          </Grid>
+          <Grid item lg={12}>
+            <Typography variant="h3" className={classes.titleTypo}>
+              Donni Septian Silalahi 
+            </Typography>
+            <Typography variant="h4" className={classes.subtitle}>
+              CEO / Matahari
+            </Typography>
+          </Grid>
+          <Grid item lg={12}>
+            {socialMedias.map((sm)=>
+            <a key={sm.link} href={sm.link} className={classes.socialMediaBtn}>
+              {sm.icons}
+            </a>
+            )}
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item lg={6}>
+          <img
+          src={ProfileImg}
+          alt="Dons"
+          width="655"
+          />
       </Grid>
     </Grid>
   )
